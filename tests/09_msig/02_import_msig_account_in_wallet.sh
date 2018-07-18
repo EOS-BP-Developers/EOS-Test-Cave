a21855d0f7c2507acaa19b3115268b4fcbb2d82a
Ubuntu <jae.chung99@gmail.com>
2018-07-18 02:56:01 +0000

    Additional fixes for v1.1.0

--- tests/09_msig/02_import_msig_account_in_wallet.sh
+++ tests/09_msig/02_import_msig_account_in_wallet.sh
@@ -21,7 +21,7 @@ for NAME in "${accounts[@]}"; do
       echo $WALLET_PASS > "$GLOBALPATH/log/wallet_name_"$NAME"_password.dat"
   fi
 
-  CMD=$( $GLOBALPATH/bin/cleos.sh wallet import -n $NAME $PRIV_KEY 2> $tpm_stderr )
+  CMD=$( $GLOBALPATH/bin/cleos.sh wallet import -n $NAME --private-key $PRIV_KEY 2> $tpm_stderr )
   ERR=$(cat $tpm_stderr)
   CHK_KEY=$(echo $CMD | awk '{print $5}')
   if [[ $ERR != "" ]]; then
