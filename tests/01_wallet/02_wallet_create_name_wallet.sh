31ed440d1f3e11d00b518b8d7cd4580c61e9f3da
Jae Chung <jae.chung99@gmail.com>
2018-08-16 01:46:55 +0800

    Update 02_wallet_create_name_wallet.sh

--- tests/01_wallet/02_wallet_create_name_wallet.sh
+++ tests/01_wallet/02_wallet_create_name_wallet.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
-################################################################################
-#
-# EOS Testing cave
-#
-# Created by Bohdan Kossak
-# 2018 CryptoLions.io
-#
-# For automated testing EOS software
-#
-# Git Hub: https://github.com/CryptoLions
-# Eos Network Monitor: http://eosnetworkmonitor.io/
-#
-#
-###############################################################################
-
-
 
 TEST_NAME="Create wallet with name $NAME"
 
@@ -24,7 +8,7 @@ NAME="$( jq -r '.wallet_test_name' "$config" )"
 
 #----------------------------------
 
-CMD=$($GLOBALPATH/bin/cleos.sh wallet create -n $NAME 2>$tpm_stderr)
+CMD=$($GLOBALPATH/bin/cleos.sh wallet create -n $NAME --to-console 2>$tpm_stderr)
 
 ERR=$(cat $tpm_stderr)
 
